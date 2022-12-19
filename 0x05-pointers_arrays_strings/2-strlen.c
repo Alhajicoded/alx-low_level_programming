99a91a5ed7049055df6984ed057924931711a4bc
Alhajicoded <babxcoded5@gmail.com>
2022-12-19 07:20:54 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - to return the length of string
+ * @s: char to check
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int a = 0; /* start the counter from 0*/
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
